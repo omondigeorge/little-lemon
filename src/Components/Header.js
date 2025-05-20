e5b44ea3772cdbd0aef0420396323d07d5ac5718
George Omondi <omondigeorge.r@gmail.com>
2025-05-21 00:59:38 +0300

    Created components for the web-app

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return(
+        <>
+        <img src="../public/little-lemon-logo.png" alt="Little-Lemon Logo"/>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/Components/Nav.js
@@ -0,0 +1,16 @@
+function Nav() {
+    return(
+        <>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+            <li>Login</li>
+        </ul>
+        </>
+    )
+}
+
+export default Nav;
