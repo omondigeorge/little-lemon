342f01f522cd60cf4da27c3dd2b07a45b40f7236
George Omondi <omondigeorge.r@gmail.com>
2025-05-21 04:07:58 +0300

    Added all the code

--- src/Components/Header.js
+++
@@ -1,9 +0,0 @@
-function Header() {
-    return(
-        <>
-        <img src="../public/little-lemon-logo.png" alt="Little-Lemon Logo"/>
-        </>
-    )
-}
-
-export default Header;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import ScrollToTop from './components/layout/ScrollToTop';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ScrollToTop />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
